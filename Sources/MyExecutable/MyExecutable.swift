45499592c409d9737b3f0527af94815b17ce70a2
Mateus Rodrigues <mateus.fr26@gmail.com>
2022-03-18 18:13:39 +0000

    Add URLSession example

--- Sources/MyExecutable/MyExecutable.swift
+++ Sources/MyExecutable/MyExecutable.swift
@@ -1,8 +1,30 @@
+import Foundation
+import FoundationNetworking
+
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
+}
+
 @main
 public struct MyExecutable {
     
     public static func main() async {
-        print("Hello, World!")
+        
+        let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song")!
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            let response = try JSONDecoder().decode(Response.self, from: data)
+            print(response.results.count)   
+        } catch {
+            print(error)
+        }
+
     }
     
 }
